c7ef92f7f500b8d2ecf5f149a6376338cc400991
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-15 15:33:41 +0530

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,56 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.*;
+public class GamePanel extends JPanel implements Runnable{
+public static int WIDTH = 400;
+public static int HEIGHT =400;
+
+private Thread thread;
+private boolean running;
+
+private BufferedImage image;
+private Graphics2D g;
+
+
+public GamePanel(){
+super(); 
+setPreferredSize(new Dimension (WIDTH ,HEIGHT));
+setFocusable(true);
+requestFocus();
+}
+
+public void addNotify(){
+super.addNotify();
+if(thread== null){thread = new Thread(this) ;
+thread.start();
+}
+
+public void run(){ 
+  running = true ;
+  image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+  g = (Graphics2D)image.getGraphics();
+           //game loop
+               while(running){
+                         gameUpdate();
+                         gameRender();
+                         gameDraw();
+                        }
+                 }
+
+private void gameUpdate(){}
+
+
+private void gameRender(){g.setColor(Color.WHITE); 
+g.fillRect(0,0,WIDTH,HEIGHT);
+g.setColor(Color.BLACK);
+g.drawString("TEST",100,100);}
+
+
+
+
+private void gameDraw(){
+Graphics g2 =this.getGraphics();
+g2.drawImage(image,0,0,null);
+g2.dispose();
+}
+}
