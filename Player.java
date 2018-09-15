9b47965132c76e6035f3fc296c501d66dcf14452
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-15 18:57:28 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,13 @@
+import java.awt.*; // for the graphics 
+public class Player{
+      //FEILDS
+      int x;
+      int y ;
+      int r ; // radius
+      
+      int dx;
+      int dy ;
+      int speed;
+    
+      
+}
