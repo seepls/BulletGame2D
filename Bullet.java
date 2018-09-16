86ced502ea889653260c6bb8ee246359aa1461cb
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-16 09:00:04 +0530

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -25,6 +25,19 @@ public Bullet(double angle , int  x ,int  y){ // giving as parameters angle and 
   
   }
 //FUNCTIONS 
-
+public boolean  update(){
+  x+= dx ;
+  y+=dy;
+  if(x<-r || x>GamePanel.WIDTH +r || y<-r|| y>GamePanel.HEIGHT +r){
+    return(true);
+  }
+  return (false);
+}
+  
+public void draw(Graphics2D g){
+  g.setColor(color1);
+  g.fillOval((int)(x-r), (int)(y-r) ,2*r ,2*r);
+  
+}
 
 }
