f4d9a9f90b66933e9278bd6d4629733323f6a333
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-16 17:38:43 +0530

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+public class Enemy{
+   //FEILDS
+   private double x;
+   private double y;
+   private int r ;
+   
+   private double dx;
+   private double dy;
+   private double rad;
+   private double speed ;
+   
+   private int health ;
+   private int type ;
+   private int rank ;
+   
+   private Color color1;
+   
+   private boolean ready ;
+   private boolean dead;
+   
+   
+   //CONSTRUCTOR 
+   
+}
