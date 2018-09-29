b9c3225aa3508a7a5ee064fdbaf3abd9b83a1f16
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-09-30 01:21:01 +0530

    Update Explosion.java

--- Explosion.java
+++ Explosion.java
@@ -1,4 +1,28 @@
 import java.awt.*;
 public class Explosion{
-
+  private double x ;
+  private double y ;
+  private int r ;
+  private int maxRadius ;
+  
+  public Explosion (double x , double y , int r ,int max){
+    this.x = x ;
+    this.y = y ;
+    this.r = r ;
+    maxRadius = max ;        
+  }
+  
+ public void update(){
+   r++;
+   if(r>= maxRadius){
+     return true;
+   }
+   return false;
+ }
+  
+  public void draw (Graphics2D g){
+    g.setColor(new Color(255 , 255, 255 ,128));
+    g.drawOval((int)(x-r) , (int ) (y-r) ,2*r , 2*r) ;
+    
+  }
 }
